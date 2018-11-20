5398fbbb09b94d221fd710fc0262a74fb9110df7
KitKat <katherine_nguyen7@yahoo.com>
2018-11-19 23:12:56 -0800

    initial

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,28 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// Fade in Heading Links //
+$(document).ready(function() {
+
+    // Displays About section on click //
+    $("#about").on("click", function() {
+      
+    })
+
+    // Displays Portfolio section on click //
+    $("#portfolio").on("click", function() {
+       
+    })
+
+    // Displays Contact section on click //
+    $("#contact").on("click", function() {
+      
+    })
+
+});
+
